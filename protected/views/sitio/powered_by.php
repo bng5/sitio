1673699629a7b6b6311c58cae5486878f076c4d6
Pablo Bangueses <pablobngs@gmail.com>
2014-01-08 13:09:19 -0200

    Potenciado por...

--- protected/views/sitio/powered_by.php
+++ protected/views/sitio/powered_by.php
@@ -1,20 +1,15 @@
-<?php
 
-Yii::app()->clientScript->registerCss('logos', "
-li {
-    float: left;
-    list-style-type: none;
-    margin: 0 15px 15px 0;
-}
-");
-?>
 <h1>Potenciado por</h1>
-<ul style="margin: 0; overflow: hidden; padding: 0;">
+<ul class="logos">
     <li><a href="http://www.centos.org/" rel="external"><img src="/img/poweredby/centos" alt="CentOS" title="CentOS" /></a></li>
+    <li><a href="http://httpd.apache.org/" rel="external"><img src="/img/poweredby/apache" alt="Apache HTTP Server" title="Apache HTTP Server" /></a></li>
     <li><a href="http://php.net/" rel="external"><img src="/img/poweredby/php" alt="PHP" title="PHP" /></a></li>
-    <li><a href="" rel="external"><img src="/img/poweredby/yii" alt="Yii Framework" title="Yii Framework" /></a></li>
-    <li><a href="" rel="external"><img src="/img/poweredby/couchdb" alt="CouchDB" title="CouchDB" /></a></li>
+    <li><a href="http://www.yiiframework.com/" rel="external"><img src="/img/poweredby/yii" alt="Yii Framework" title="Yii Framework" /></a></li>
+    <li><a href="http://couchdb.apache.org/" rel="external"><img src="/img/poweredby/couchdb" alt="CouchDB" title="CouchDB" /></a></li>
 </ul>
 
-<a href="http://www.famfamfam.com/lab/icons/silk/" rel="external">Silk Icons</a>
+<ul>
+    <li>Íconos: <a href="http://www.famfamfam.com/lab/icons/silk/" rel="external">Silk Icons</a></li>
+    <li>Resaltado de código: <a href="http://qbnz.com/highlighter/" rel="external">GeSHi - Generic Syntax Highlighter</a></li>
+</ul>
 
